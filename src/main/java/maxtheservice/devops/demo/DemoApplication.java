2754028f528b67c5b6094ae63c231a9063db16aa
ShahidHussainAbbasi <uncer_sh@yahoo.com>
2024-12-03 13:38:12 +0500

    abc commit

--- src/main/java/maxtheservice/devops/demo/DemoApplication.java
+++ src/main/java/maxtheservice/devops/demo/DemoApplication.java
@@ -13,7 +13,7 @@ public class DemoApplication {
       SpringApplication.run(DemoApplication.class, args);
     }
     @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "123") String name) {
+    public String hello(@RequestParam(value = "name", defaultValue = "abc") String name) {
       return String.format("Hello New %s!", name);
     }
 }
