2f6c5b07b55b851620421a4ce8f39658f272eb59
ShahidHussainAbbasi <uncer_sh@yahoo.com>
2024-12-14 23:31:22 +0500

    added dockerfiel to run application as a container

--- src/main/java/maxtheservice/devops/demo/DemoApplication.java
+++ src/main/java/maxtheservice/devops/demo/DemoApplication.java
@@ -13,7 +13,7 @@ public class DemoApplication {
       SpringApplication.run(DemoApplication.class, args);
     }
     @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "abc") String name) {
-      return String.format("Hello New %s!", name);
+    public String hello(@RequestParam(value = "name", defaultValue = "demo") String name) {
+      return String.format("Hello %s!", name);
     }
 }
